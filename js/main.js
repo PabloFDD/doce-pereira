1a15467cc79850cde5f54e113f9f3be7e00bdd80
PabloFDD <pablofdelboni@gmail.com>
2022-08-31 16:26:51 -0300

    Adicionado novamente o carrousel

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
-// $(function(){
-// 	$(".produtos__carrousel").slick({
-//         dots: true,
-//         infinite: true,
-//         speed: 300,
-//         slidesToShow: 1,
-//         centerMode: true,
-//         variableWidth: true,
-//         autoplay: true,
-//         autoplaySpeed: 2000
-//     });
-// });
+$(function(){
+	$(".area-atuacao__mapa").slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+});
